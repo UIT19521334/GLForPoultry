58651310afe77b523e834f6a148602aafcd3289f
NDCDat <67258104+UIT19521334@users.noreply.github.com>
2025-09-11 11:41:25 +0700

    setItem

--- src/DomainApi/index.js
+++ src/DomainApi/index.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
-
-console.log('API URL:', process.env.REACT_APP_API_URL);
-
 export default axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}`,
+    baseURL: `${process.env.REACT_APP_API_URL_POULTRY}`,
     headers: {
         'Content-Type': 'application/json',
     },
