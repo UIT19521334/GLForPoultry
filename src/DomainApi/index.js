99ef104bb1607ba9d2f485bb29d157291f7fce5a
NDCDat <67258104+UIT19521334@users.noreply.github.com>
2025-09-11 14:15:12 +0700

    update

--- src/DomainApi/index.js
+++ src/DomainApi/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log('API URL:', process.env.REACT_APP_API_URL);
-
 export default axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
     headers: {
